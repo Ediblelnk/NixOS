349a8775befcb3b4cc3e085171e3b75c05a06bbe
Ediblelnk <peters1731@gmail.com>
2025-05-26 10:09:41 -0400

    base

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+    description = "EdibleInk's NixOS flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"
+    };
+
+    outputs = { self, nixpkgs, ... }@inputs:
+    let
+        lib = nixpkgs.lib;
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        nixosConfigurations.nixos = lib.nixosSystem {
+            inherit system;
+            modules = [ ./configuration.nix ];
+        };
+    };
+}
