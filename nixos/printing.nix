7cb894ec3e15d0f79b196e1bfcac30a58d461832
Ediblelnk <peters1731@gmail.com>
2025-08-15 11:46:29 -0400

    network printing

--- nixos/printing.nix
+++ nixos/printing.nix
@@ -1,4 +1,10 @@
 { config, pkgs, ... }: {
   # Set your time zone.
   services.printing.enable = true;
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
