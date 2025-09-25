f38751c26202215b6d577ed114beee7d09a9503c
Ediblelnk <peters1731@gmail.com>
2025-09-25 09:41:24 -0400

    wallpaper script

--- .config/hypr/scripts.wallpaper.sh
+++ .config/hypr/scripts.wallpaper.sh
@@ -6,5 +6,17 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
+ln -s -f "$WALLPAPER" "$HOME/.wallpaper"
+
 # Apply the selected wallpaper
 hyprctl hyprpaper reload ,"$WALLPAPER"
+
+wal -i $WALLPAPER
+
+~/.config/wal-brave/generate-theme.sh
+
+pkill waybar && waybar &
+
+echo "Wallpaper changed to $WALLPAPER!"
+
+ln -s -f "$WALLPAPER" "$HOME/.wallpaper"
