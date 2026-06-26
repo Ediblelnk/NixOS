ea30a86377390899de4f197292c348c060a0d605
Ediblelnk <peters1731@gmail.com>
2026-06-26 01:25:00 -0400

    [Hyprland] Layer animations -> fade

--- .config/hypr/settings/animations.lua
+++ .config/hypr/settings/animations.lua
@@ -12,26 +12,32 @@ hl.curve("circ",  { type = "bezier", points = { { 0, 0.5 },     { 0.5, 1 } } })
 hl.animation({ leaf = "global", enabled = true, speed = 1, bezier = "circ" })
 --
 hl.animation({ leaf = "windowsIn", enabled = true, speed = 1, bezier = "circ", style = "slide" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 1, bezier = "circ", style = "gnomed" })
+hl.animation({ leaf = "windowsOut", enabled = false, speed = 1, bezier = "circ", style = "gnomed" })
 --
-hl.animation({ leaf = "fadeIn", enabled = false })
-hl.animation({ leaf = "fadeOut", enabled = true, speed = 10, bezier = "cubic" })
+hl.animation({ leaf = "fadeIn", enabled = true, speed = 1, bezier = "cubic" })
+hl.animation({ leaf = "fadeOut", enabled = true, speed = 2, bezier = "circ" })
 hl.animation({ leaf = "fade", enabled = true, speed = 1, bezier = "cubic" })
 --
-hl.animation({ leaf = "layersIn", enabled = true, speed = 1, bezier = "cubic", style = "slide" })
-hl.animation({ leaf = "layersOut", enabled = true, speed = 1, bezier = "cubic", style = "slide" })
+hl.animation({ leaf = "layersIn", enabled = true, speed = 1, bezier = "circ", style = "fade" })
+hl.animation({ leaf = "layersOut", enabled = true, speed = 1, bezier = "circ", style = "fade" })
 --
-hl.animation({ leaf = "fadeLayersIn", enabled = false })
-hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 10, bezier = "cubic" })
+hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1, bezier = "circ" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1, bezier = "circ" })
 --
-hl.animation({ leaf = "fadePopupsIn", enabled = false })
-hl.animation({ leaf = "fadePopupsOut", enabled = true, speed = 10, bezier = "cubic" })
+hl.animation({ leaf = "fadePopupsIn", enabled = true, speed = 1, bezier = "circ" })
+hl.animation({ leaf = "fadePopupsOut", enabled = true, speed = 1, bezier = "circ" })
 --
 hl.animation({ leaf = "workspaces", enabled = true, speed = 2, bezier = "circ", style = "slide" })
 hl.animation({ leaf = "specialWorkspace", enabled = true, speed = 2, bezier = "circ", style = "fade" })
 
 hl.layer_rule({
     name = "notifications animation",
-    match = { namespace = "swaync-notification-window" },
-    animation = "layersIn"
+    match = { namespace = "rofi" },
+    animation = "slide bottom"
+})
+
+hl.layer_rule({
+    name = "notifications animation",
+    match = { namespace = "swaync-control-center" },
+    animation = "slide left"
 })
