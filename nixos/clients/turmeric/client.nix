a60424a6bce4a9146bc2dfb892ea1c58844b16a4
Ediblelnk <peters1731@gmail.com>
2026-06-26 16:50:43 -0400

    [Fix] Fixed Zed complaining about vulkan / opengl

--- nixos/clients/turmeric/client.nix
+++ nixos/clients/turmeric/client.nix
@@ -1,12 +1,23 @@
-{ ... }:
+{ config,... }:
 {
   imports = [ ./hardware-configuration.nix ];
   networking.hostName = "turmeric";
 
   # because of a NVIDIA GPU on this machine
   hardware = {
-    nvidia.modesetting.enable = true;
-    nvidia.open = true;
+    graphics.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      open = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  environment.variables = {
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_DRM_DEVICES = "/dev/dri/card1"; # Adjust card IDs as needed
+    WLR_NO_HARDWARE_CURSORS = "1"; # Optional: fixes cursor issues on some setups
+  };
 }
