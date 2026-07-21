24a11212436bf4f2208915f36f0c0cbd433113ac
Ediblelnk <peters1731@gmail.com>
2026-07-21 15:13:27 -0400

    [Hyprland] Add steam to autostart

--- .config/hypr/settings/autostart.lua
+++ .config/hypr/settings/autostart.lua
@@ -11,10 +11,13 @@ hl.on("hyprland.start", function()
     end
     hl.exec_cmd("waybar")
     hl.exec_cmd("quickshell")
-    hl.exec_cmd("blueman-applet")
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("swaync")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("gnome-keyring-daemon --daemonize")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
+
+    -- tray programs
+    hl.exec_cmd("blueman-applet")
+    hl.exec_cmd("steam -silent")
 end)
