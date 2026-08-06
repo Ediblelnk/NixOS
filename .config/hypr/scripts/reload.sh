ff97c57ed00faffa753fc678c9302703e69f2dde
Ediblelnk <peters1731@gmail.com>
2026-08-05 23:30:50 -0400

    [Swaync] Scaffold out action buttons + more css

--- .config/hypr/scripts/reload.sh
+++ .config/hypr/scripts/reload.sh
@@ -4,8 +4,7 @@ reload() {
     # Restart background services detached so they don't inherit the caller's terminal
     swaync-client --reload-css
 
-    pkill waybar >/dev/null 2>&1 || true
-    setsid waybar >/dev/null 2>&1 &
+    pkill -SIGUSR2 waybar
 
     # pkill quickshell >/dev/null 2>&1 || true
     # setsid quickshell >/dev/null 2>&1 &
